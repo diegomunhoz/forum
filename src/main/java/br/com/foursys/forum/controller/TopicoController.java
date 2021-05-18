624d64cedfe4bf504aa0eccdd010cd79969bd701
Diego Vinicius De Mello Munhoz <dmunhoz@server01.foursys.local>
2021-05-18 10:31:00 -0300

    implementação POST TopicoController

--- src/main/java/br/com/foursys/forum/controller/TopicoController.java
+++ src/main/java/br/com/foursys/forum/controller/TopicoController.java
@@ -1,13 +1,21 @@
 package br.com.foursys.forum.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.foursys.forum.controller.dto.TopicoDto;
+import br.com.foursys.forum.controller.form.TopicoForm;
 import br.com.foursys.forum.model.Topico;
+import br.com.foursys.forum.repository.CursoRepository;
 import br.com.foursys.forum.repository.TopicoRepository;
 
 /**
@@ -18,21 +26,35 @@ import br.com.foursys.forum.repository.TopicoRepository;
  * @version 1.0
  */
 @RestController
+@RequestMapping("/topicos")
 public class TopicoController {
-	
+
 	@Autowired
 	private TopicoRepository topicoRepository;
 
-	@RequestMapping("/topicos")
-	public List<TopicoDto> lista(String nomeCurso){
+	@Autowired
+	private CursoRepository cursoRepository;
+
+	@GetMapping
+	public List<TopicoDto> listar(String nomeCurso) {
+		List<Topico> topicos = null;
+
 		if (nomeCurso == null) {
-			List<Topico> topicos = topicoRepository.findAll();
-			return TopicoDto.converter(topicos);
-		}else {
-			List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
-			return TopicoDto.converter(topicos);
+			topicos = topicoRepository.findAll();
+		} else {
+			topicos = topicoRepository.findByCursoNome(nomeCurso);
 		}
-		
+
+		return TopicoDto.converter(topicos);
 	}
-	
+
+	@PostMapping
+	public ResponseEntity<TopicoDto> cadastrar(@RequestBody TopicoForm form, UriComponentsBuilder uriBuilder) {
+		Topico topico = form.converter(cursoRepository);
+		topicoRepository.save(topico);
+
+		URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
+		return ResponseEntity.created(uri).body(new TopicoDto(topico));
+	}
+
 }
--- src/main/java/br/com/foursys/forum/repository/CursoRepository.java
+++ src/main/java/br/com/foursys/forum/repository/CursoRepository.java
@@ -6,4 +6,6 @@ import br.com.foursys.forum.model.Curso;
 
 public interface CursoRepository extends JpaRepository<Curso, Long> {
 
+	Curso findByNome(String nome);
+
 }
